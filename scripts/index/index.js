8b55c79ac00b3291873840543d76fefb37b66c79
Ilya.Pozh <ilya.pojidaev@yandex.ua>
2020-10-16 17:30:22 +0200

    First check/fixes

--- scripts/index/index.js
+++ scripts/index/index.js
@@ -3,49 +3,32 @@ const buttonClosePopup = document.querySelector('.popup__close-button');
 const popup = document.querySelector('.popup');
 let formElement = popup.querySelector('.popup__container');
 
+let nameInput = formElement.querySelector('.popup__input_content_name');
+let jobInput = formElement.querySelector('.popup__input_content_job');
+let profileName = document.querySelector('.profile__user-name');
+let profileJob = document.querySelector('.profile__profession');
 
 
 const popupToggle = function () {
     popup.classList.toggle('popup_opened');
 }
 
-// Находим форму в DOM
-// Воспользуйтесь методом querySelector)
 
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
 function formSubmitHandler (evt) {
     evt.preventDefault(); 
 
-    // Находим поля формы в DOM
-    let nameInput = formElement.querySelector('.popup__input_content_name');
-    let jobInput = formElement.querySelector('.popup__input_content_job');
-
-    // Получите значение полей из свойства value
-    console.log(nameInput.value);
-    console.log(jobInput.value);
-
-    // Выберите элементы, куда должны быть вставлены значения полей
-    let profileName = document.querySelector('.profile__userName');
-    let profileJob = document.querySelector('.profile__profession');
-
-    // Вставьте новые значения с помощью textContent
     profileName.textContent = nameInput.value;
     profileJob.textContent = jobInput.value;
+    
+    
 }
 
 function copyText () {
 
-    let profileName = document.querySelector('.profile__userName');
-    let profileJob = document.querySelector('.profile__profession');
-    let nameInput = formElement.querySelector('.popup__input_content_name');
-    let jobInput = formElement.querySelector('.popup__input_content_job');
-
-    console.log(profileName.textContent);
-    console.log(profileJob.textContent);
-
     nameInput.value = profileName.textContent;
     jobInput.value = profileJob.textContent;
+
+    
 }
 
 
